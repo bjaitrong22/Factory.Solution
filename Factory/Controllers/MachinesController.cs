41c753be125d34a01d889b68fe8d67ec1e5b1d75
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-15 00:31:59 -0700

    Add AssignAnEngineer action, view, and update links.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -96,5 +96,29 @@ namespace Factory.Controllers
 
       return RedirectToAction("Index");
     }
+
+    public ActionResult AssignAnEngineer(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "LastName");
+
+        return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult AssignAnEngineer(Machine machine, int engineerId)
+    {
+      #nullable enable
+      EngineerMachine? joinEntity = _db.EngineerMachines.FirstOrDefault( join => (join.EngineerId == engineerId && join.MachineId == machine.MachineId));
+      #nullable disable
+
+      if (joinEntity == null && engineerId != 0)
+        {
+          _db.EngineerMachines.Add(new EngineerMachine() { EngineerId = engineerId, MachineId = machine.MachineId });
+          _db.SaveChanges();
+        }
+
+      return RedirectToAction("Details", new { id = machine.MachineId});
+    }
   }
 }
